f83422f175e49df0ef0c5d62b741819ef973e049
choi-myung-gyu <cmg080137@gmail.com>
2023-09-11 11:18:50 +0900

    Question1 : 자릿수더하기

---
+++ src/programmers/Question_1.java
@@ -0,0 +1,29 @@
+package programmers;
+
+public class Question_1
+{
+    //문자형 변형
+    public int solution(int n) {
+        int answer = 0;
+        String m = Integer.toString(n);
+
+        for(int i = 0; i<m.length(); i++){
+            answer += Integer.parseInt(m.substring(i, i+1));
+        }
+
+        return answer;
+    }
+
+    //다른 풀이 정수 나눗셈
+    public int solution2(int n2) {
+        int answer2 = 0;
+
+        while(n2 < 0){
+            answer2 += n2%10;
+            if(n2 < 10)
+                break;
+            n2=n2/10;
+        }
+        return answer2;
+    }
+}
