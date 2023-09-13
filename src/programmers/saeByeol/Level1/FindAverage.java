15004858612a0443afe43036295c960cc6ecb2cc
Saebyeol <94090893+saebyeolKim@users.noreply.github.com>
2023-09-14 08:52:33 +0900

    add: 평균 구하기

---
+++ src/programmers/saeByeol/Level1/FindAverage.java
@@ -0,0 +1,29 @@
+package programmers.saeByeol.Level1;
+
+/**
+ * 평균 구하기
+ *
+ * 문제 설명
+ * 정수를 담고 있는 배열 arr의 평균값을 return하는 함수, solution을 완성해보세요.
+ *
+ * 제한사항
+ * arr은 길이 1 이상, 100 이하인 배열입니다.
+ * arr의 원소는 -10,000 이상 10,000 이하인 정수입니다.
+ */
+public class FindAverage {
+
+    public double solution(int[] arr) {
+        double answer = 0;
+        for (int a : arr) {
+            answer += a;
+        }
+        return answer / arr.length;
+    }
+
+    public static void main(String[] args) {
+        FindAverage findAverage = new FindAverage();
+        int[] arr = {5,5};
+        double solution = findAverage.solution(arr);
+        System.out.println("solution = " + solution);
+    }
+}
