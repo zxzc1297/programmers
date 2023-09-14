45876213b6911c8ed93f3814fa24d6dcef91159b
choi-myung-gyu <cmg080137@gmail.com>
2023-09-14 09:19:30 +0900

    Question4 : 평균 구하기

--- src/programmers/myungGyu/Level1/Question_3.java
+++ src/programmers/myungGyu/Level1/Question_3.java
@@ -1,5 +1,15 @@
 package programmers.myungGyu.Level1;
 
+/**
+ * 짝수와 홀수
+ *
+ * 문제 설명
+ * 정수 num이 짝수일 경우 "Even"을 반환하고 홀수인 경우 "Odd"를 반환하는 함수, solution을 완성해주세요.
+ *
+ * 제한 조건
+ * num은 int 범위의 정수입니다.
+ * 0은 짝수입니다.
+ */
 public class Question_3 {
     //2를 나눔으로써 홀수, 짝수 구분
     public String solution(int num) {
