a65301ef127b1308a6c7ca35be7be67167af03f4
Saebyeol <94090893+saebyeolKim@users.noreply.github.com>
2023-09-15 09:13:04 +0900

    add: 나머지가 1이 되는 수 찾기

---
+++ src/programmers/saeByeol/Level1/FindRemainOne.java
@@ -0,0 +1,26 @@
+package programmers.saeByeol.Level1;
+
+/**
+ * 나머지가 1이 되는 수 찾기
+ * 문제 설명
+ * 자연수 n이 매개변수로 주어집니다. n을 x로 나눈 나머지가 1이 되도록 하는 가장 작은 자연수 x를 return 하도록 solution 함수를 완성해주세요. 답이 항상 존재함은 증명될 수 있습니다.
+ *
+ * 제한사항
+ * 3 ≤ n ≤ 1,000,000
+ */
+public class FindRemainOne {
+    public int solution(int n) {
+        int answer = 1;
+        while (n % answer != 1) {
+            answer++;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        FindRemainOne findRemainOne = new FindRemainOne();
+        int solution = findRemainOne.solution(12);
+        System.out.println("solution = " + solution);
+    }
+
+}
