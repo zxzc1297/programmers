3027fd37ef32e73b3ef2a242f95e677e79babf70
Saebyeol <94090893+saebyeolKim@users.noreply.github.com>
2023-09-26 11:11:07 +0900

    add: 없는 숫자 더하기

---
+++ src/programmers/saeByeol/Level1/Question19.java
@@ -0,0 +1,28 @@
+package programmers.saeByeol.Level1;
+/**
+ * 없는 숫자 더하기
+ * 문제 설명
+ * 0부터 9까지의 숫자 중 일부가 들어있는 정수 배열 numbers가 매개변수로 주어집니다.
+ * numbers에서 찾을 수 없는 0부터 9까지의 숫자를 모두 찾아 더한 수를 return 하도록 solution 함수를 완성해주세요.
+ *
+ * 제한사항
+ * 1 ≤ numbers의 길이 ≤ 9
+ * 0 ≤ numbers의 모든 원소 ≤ 9
+ * numbers의 모든 원소는 서로 다릅니다.
+ */
+public class Question19 {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        for (int number : numbers) {
+            answer += number;
+        }
+        return 45 - answer;
+    }
+
+    public static void main(String[] args) {
+        Question19 question19 = new Question19();
+        int[] numbers = {5,8,4,0,6,7,9};
+        int solution = question19.solution(numbers);
+        System.out.println("solution = " + solution);
+    }
+}
