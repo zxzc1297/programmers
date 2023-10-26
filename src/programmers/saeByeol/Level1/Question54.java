4120eb1dad0866cfdb8f6acd352d5675a1d5be2d
Saebyeol <94090893+saebyeolKim@users.noreply.github.com>
2023-10-26 18:07:25 +0900

    add: 소수 만들기

---
+++ src/programmers/saeByeol/Level1/Question54.java
@@ -0,0 +1,34 @@
+package programmers.saeByeol.Level1;
+
+/**
+ * 소수 만들기
+ * 문제 설명
+ * 주어진 숫자 중 3개의 수를 더했을 때 소수가 되는 경우의 개수를 구하려고 합니다.
+ * 숫자들이 들어있는 배열 nums가 매개변수로 주어질 때, nums에 있는 숫자들 중 서로 다른 3개를 골라 더했을 때 소수가 되는 경우의 개수를 return 하도록 solution 함수를 완성해주세요.
+ *
+ * 제한사항
+ * nums에 들어있는 숫자의 개수는 3개 이상 50개 이하입니다.
+ * nums의 각 원소는 1 이상 1,000 이하의 자연수이며, 중복된 숫자가 들어있지 않습니다.
+ */
+public class Question54 {
+    public int solution(int[] nums) {
+        int answer = 0;
+        int sum = 0;
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    sum = nums[i] + nums[j] + nums[k];
+                    boolean res = true;
+                    for(int l = 2; l < sum; l++){
+                        if(sum%l ==0){
+                            res = false;
+                            break;
+                        }
+                    }
+                    if(res) answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
