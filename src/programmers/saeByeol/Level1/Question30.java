46fc401a9e31031837b0569a7e499481de1be9b1
Saebyeol <94090893+saebyeolKim@users.noreply.github.com>
2023-12-22 14:53:03 +0900

    재시도: 최대공약수와 최소공배수

--- src/programmers/saeByeol/Level1/Question30.java
+++ src/programmers/saeByeol/Level1/Question30.java
@@ -26,6 +26,19 @@ public class Question30 {
         return answer;
     }
 
+    public int[] solutionAgain(int n, int m) {
+        int[] answer = new int[2];
+        int min = Math.min(n, m);
+        int max = Math.max(n, m);
+        for (int i = 1 ; i < max; i++) {
+            if (min % i == 0 && max % i == 0) {
+                answer[0] = i;
+                answer[1] = min * max / i;
+            }
+        }
+        return answer;
+    }
+
     /**
      * 다른 사람의 풀이
      */
