20c3e2de785707fe49228154ca57fb4b094f1a0a
Saebyeol <94090893+saebyeolKim@users.noreply.github.com>
2024-01-03 11:48:35 +0900

    add: 피보나치 수

---
+++ src/programmers/saeByeol/Level2/Question8.java
@@ -0,0 +1,34 @@
+package programmers.saeByeol.Level2;
+
+/**
+ * 피보나치 수
+ * 문제 설명
+ * 피보나치 수는 F(0) = 0, F(1) = 1일 때, 1 이상의 n에 대하여 F(n) = F(n-1) + F(n-2) 가 적용되는 수 입니다.
+ *
+ * 예를들어
+ *
+ * F(2) = F(0) + F(1) = 0 + 1 = 1
+ * F(3) = F(1) + F(2) = 1 + 1 = 2
+ * F(4) = F(2) + F(3) = 1 + 2 = 3
+ * F(5) = F(3) + F(4) = 2 + 3 = 5
+ * 와 같이 이어집니다.
+ *
+ * 2 이상의 n이 입력되었을 때, n번째 피보나치 수를 1234567으로 나눈 나머지를 리턴하는 함수, solution을 완성해 주세요.
+ *
+ * 제한 사항
+ * n은 2 이상 100,000 이하인 자연수입니다.
+ */
+public class Question8 {
+    public int solution(int n) {
+        //(a+b)%n = (a%n + b%n)%n;
+        int answer = 0;
+        int minusOne = 1;
+        int minusTwo = 0;
+        for (int i = 2; i <= n; i++) {
+            answer = minusOne + minusTwo;
+            minusTwo = minusOne % 1234567;
+            minusOne = answer % 1234567;
+        }
+        return answer % 1234567;
+    }
+}
